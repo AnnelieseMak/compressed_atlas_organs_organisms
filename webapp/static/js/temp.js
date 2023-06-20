700c6b24a3ab2eec5d051f6cd99ba3473b9e780c
Anneliese Mak <anneliesemak8@gmail.com>
2023-06-21 01:33:20 +1000

    rough functionality of 'collapsible' plot on dummy data

--- webapp/static/js/temp.js
+++ webapp/static/js/temp.js
@@ -1,5 +1,7 @@
 let plotData = {}
 
+let testList = []
+
 const plotTemplate = () => {
     console.log('plot template')
     var trace1 = {
@@ -23,9 +25,20 @@ const plotTemplate = () => {
         name: '',
         type: 'heatmap'
       };
+
+      var trace3 = {
+        x: [
+          ['AU Zoo','AU Zoo','AU Zoo'],
+          ['a', 'b', 'c']
+        ],
+        y: ['y1', 'y2', 'y3'],
+        z: [[0,1,2], [3,7,5], [6,null,8]],
+        name: '',
+        type: 'heatmap'
+      };
       
-      var data = [trace1, trace2];
-      console.log(data)
+      var data = [trace1, trace2, trace3];
+    //   console.log(data)
 
       var layout = {
         showlegend: false,
@@ -35,15 +48,206 @@ const plotTemplate = () => {
           showdividers: true,
           dividercolor: 'grey',
           dividerwidth: 2,
-          layer: 'above traces'
         },
         yaxis: {
             autorange: "reversed",
         }
       };
+      testList = ['SF Zoo', 'LA Zoo', 'AU Zoo']
       
       Plotly.newPlot('plotDiv', data, layout);
+
+    const xTicks = document.getElementsByClassName('xtick2')
+    for (let i = 0; i < xTicks.length; i++) {
+        console.log(xTicks[i])
+        const tickLabel = xTicks[i].children[0].innerHTML
+        xTicks[i].onclick = function(){myFunction(tickLabel)}
+    }
+
 }
+
+const myFunction = (tickLabel) => {
+    if (testList.length == 1) {
+        return
+    }
+
+    console.log(`clicked: ${tickLabel}`)
+    const labelIdx = testList.indexOf(tickLabel)
+    console.log(testList, labelIdx)
+    testList.splice(labelIdx, 1)
+    console.log(testList)
+
+    const ctListEl = document.getElementById('celltypeList')
+
+    if (ctListEl.style.display == "none") {
+        console.log('is none')
+        ctListEl.style.display = "block"
+    }
+
+    const ctNameTemplate = document.getElementById('celltypeName-template')
+    const newCT = ctNameTemplate.cloneNode(true)
+    newCT.removeAttribute('id')
+    newCT.style.display = "block"
+    newCT.innerHTML = tickLabel
+    newCT.onclick = function(){myFunction2(newCT)}
+
+    ctListEl.appendChild(newCT)
+
+
+    var trace1 = {
+        x: [
+          ['SF Zoo','SF Zoo','SF Zoo'],
+          ['giraffes', 'orangutans', 'monkeys']
+        ],
+        y: ['y1', 'y2', 'y3'],
+        z: [[10,11,112], [13,14,15], [16,17,18]],
+        name: '',
+        type: 'heatmap'
+      };
+      
+      var trace2 = {
+        x: [
+          ['LA Zoo','LA Zoo','LA Zoo'],
+          ['g', 'o', 'm']
+        ],
+        y: ['y1', 'y2', 'y3'],
+        z: [[0,1,2], [3,null,5], [6,7,8]],
+        name: '',
+        type: 'heatmap'
+      };
+
+      var trace3 = {
+        x: [
+          ['AU Zoo','AU Zoo','AU Zoo'],
+          ['a', 'b', 'c']
+        ],
+        y: ['y1', 'y2', 'y3'],
+        z: [[0,1,2], [3,7,5], [6,null,8]],
+        name: '',
+        type: 'heatmap'
+      };
+      
+    //   var data = [trace1, trace2];
+    const data = []
+    if (testList.includes('SF Zoo')) {
+        data.push(trace1)
+    }
+    if (testList.includes('LA Zoo')) {
+        data.push(trace2)
+    }
+    if (testList.includes('AU Zoo')) {
+        data.push(trace3)
+    }
+
+    var layout = {
+        showlegend: false,
+        xaxis: {
+          tickson: "boundaries",
+          ticklen: 15,
+          showdividers: true,
+          dividercolor: 'grey',
+          dividerwidth: 2,
+        },
+        yaxis: {
+            autorange: "reversed",
+        }
+      };
+
+    Plotly.newPlot('plotDiv', data, layout);
+
+    const xTicks = document.getElementsByClassName('xtick2')
+    for (let i = 0; i < xTicks.length; i++) {
+        console.log(xTicks[i])
+        const tickLabel = xTicks[i].children[0].innerHTML
+        xTicks[i].onclick = function(){myFunction(tickLabel)}
+    }
+
+}
+
+const myFunction2 = (labelElement) => {
+    console.log('add to plot')
+    // console.log(labelElement)
+    
+    const ctListParent = document.getElementById('celltypeList')
+    testList.push(labelElement.innerHTML)
+    labelElement.remove()
+
+    if (ctListParent.childElementCount == 1) {
+        ctListParent.style.display = "none"
+    }
+
+    var trace1 = {
+        x: [
+          ['SF Zoo','SF Zoo','SF Zoo'],
+          ['giraffes', 'orangutans', 'monkeys']
+        ],
+        y: ['y1', 'y2', 'y3'],
+        z: [[10,11,112], [13,14,15], [16,17,18]],
+        name: '',
+        type: 'heatmap'
+      };
+      
+      var trace2 = {
+        x: [
+          ['LA Zoo','LA Zoo','LA Zoo'],
+          ['g', 'o', 'm']
+        ],
+        y: ['y1', 'y2', 'y3'],
+        z: [[0,1,2], [3,null,5], [6,7,8]],
+        name: '',
+        type: 'heatmap'
+      };
+
+      var trace3 = {
+        x: [
+          ['AU Zoo','AU Zoo','AU Zoo'],
+          ['a', 'b', 'c']
+        ],
+        y: ['y1', 'y2', 'y3'],
+        z: [[0,1,2], [3,7,5], [6,null,8]],
+        name: '',
+        type: 'heatmap'
+      };
+      
+    //   var data = [trace1, trace2];
+    const data = []
+    if (testList.includes('SF Zoo')) {
+        data.push(trace1)
+    }
+    if (testList.includes('LA Zoo')) {
+        data.push(trace2)
+    }
+    if (testList.includes('AU Zoo')) {
+        data.push(trace3)
+    }
+
+    var layout = {
+        showlegend: false,
+        xaxis: {
+          tickson: "boundaries",
+          ticklen: 15,
+          showdividers: true,
+          dividercolor: 'grey',
+          dividerwidth: 2,
+        },
+        yaxis: {
+            autorange: "reversed",
+        }
+      };
+
+    Plotly.newPlot('plotDiv', data, layout);
+
+    const xTicks = document.getElementsByClassName('xtick2')
+    for (let i = 0; i < xTicks.length; i++) {
+        console.log(xTicks[i])
+        const tickLabel = xTicks[i].children[0].innerHTML
+        xTicks[i].onclick = function(){myFunction(tickLabel)}
+    }
+}
+
+/*****************************************************************************
+ *                              USED METHODS
+ *****************************************************************************/
 
 const generatePlot = async () => {
     console.log('generate plot')
@@ -133,18 +337,30 @@ const generatePlot = async () => {
     // Plotly.newPlot('plotDiv', testData, layout);
 
     Plotly.newPlot('plotDiv', traceData, layout);
-    
-    const xTicks = document.getElementsByClassName('xtick2')
-    console.log(xTicks)
 
-    for(let i = 0; i < xTicks.length; i++) {
-        console.log(xTicks[i])
-        xTicks[i].style.color = 'red'
-        xTicks[i].onclick = function() {
-            // console.log(`xTick: ${xTicks[i].textContent}`)
-            console.log('clicked')
-        }
+
+    const xTicks = document.getElementsByClassName('xtick2')
+
+    console.log(`xTicks`)
+    console.log(xTicks)
+    for (let i = 0; i < xTicks.length; i++) {
+        const tickLabel = xTicks[i].children[0].innerHTML
+        xTicks[i].onclick = function(){collapsePlot(tickLabel)}
     }
+
+
+    
+    // const xTicks = document.getElementsByClassName('xtick2')
+    // console.log(xTicks)
+
+    // for(let i = 0; i < xTicks.length; i++) {
+    //     console.log(xTicks[i])
+    //     xTicks[i].style.color = 'red'
+    //     xTicks[i].onclick = function() {
+    //         // console.log(`xTick: ${xTicks[i].textContent}`)
+    //         console.log('clicked')
+    //     }
+    // }
 
     // EACH TRACE IS FOR 1 CELL TYPE
     // trace
@@ -165,19 +381,9 @@ const generatePlot = async () => {
 
 }
 
-// const tt = document.getElementsByClassName('apples')
-// console.log(tt)
-// for (let i = 0; i < tt.length; i++) {
-//     console.log(tt[i])
-//     tt[i].onclick = function() {
-//         console.log('tt')
-//     }
-// }
-
-window.onclick = e => {
-    console.log(e.target);  // to get the element
-    console.log(e.target.tagName);  // to get the element tag name alone
-} 
+const collapsePlot = (tickLabel) => {
+    console.log(`collapse plot: ${tickLabel}`)
+}
 
 // return unique celltypes
 const getAllCellTypes = (data) => {
